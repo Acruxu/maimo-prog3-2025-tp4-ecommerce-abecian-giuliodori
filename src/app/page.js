f00f67865080ff4af18b569150a91ce41ecbaff2
Acruxu <giuliodori.juancruz03@gmail.com>
2025-08-28 16:16:16 -0300

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+    <nav>
+      <ul>
+        <li>
+          about
+        </li>
+      </ul>
+    </nav>
    <div>Hla</div>
+   </>
   );
 }
