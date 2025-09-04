a79814e6d98fc9ae5accc2762c1ef9d5e4ae3421
Acruxu <giuliodori.juancruz03@gmail.com>
2025-09-03 21:14:32 -0300

    agregando categorias

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Footer from "./components/Footer";
+import PrincipalCategories from "./components/PrincipalCategories";
 
 export default function Home() {
   return (
+    <>
+    <PrincipalCategories></PrincipalCategories>
     <Footer></Footer>
+    </>
   );
 }
